fd2953ed25c4beafb7069b74fdeb7feadc7822ea
Bruno <bruno.deprez@gmail.com>
2013-01-30 18:49:25 +0000

    Added Sailing Boat Ad

---
+++ bea.dal/map/Ads/SailingBoatAdMap.cs
@@ -0,0 +1,37 @@
+﻿using Bea.Domain;
+using Bea.Domain.Ads.WaterSport;
+using Bea.Domain.Categories;
+using Bea.Domain.Location;
+using Bea.Domain.Reference;
+using FluentNHibernate.Mapping;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Bea.Dal.Map.Ads
+{
+    public class SailingBoatAdMap : SubclassMap<SailingBoatAd>
+    {
+        public SailingBoatAdMap()
+        {
+            Map(x => x.Title).Not.Nullable();
+            Map(x => x.Body).Not.Nullable();
+            Map(x => x.CreationDate).Not.Nullable();
+            Map(x => x.Price).Nullable();
+            Map(x => x.IsOffer).Not.Nullable();
+            Map(x => x.PhoneNumber);
+
+            References<User>(x => x.CreatedBy).Not.Nullable();
+            References<City>(x => x.City).Not.Nullable();
+            References<Category>(x => x.Category).Not.Nullable();
+
+
+            Map(x => x.Length);
+            Map(x => x.Year);
+
+            References<SailingBoatType>(x => x.Type);
+            References<SailingBoatHullType>(x => x.HullType);
+        }
+    }
+}
---
+++ bea.domain/Ads/WaterSport/Engine.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Bea.Domain.Ads.WaterSport
+{
+    public class Engine
+    {
+        public virtual 
+    }
+}
