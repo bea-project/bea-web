20e0c2049ba6d00972c33c89b6216a88b28def7d
Bruno <bruno.deprez@gmail.com>
2013-04-23 11:32:50 +1100

    Added FaceBook Like Button to Ad

---
+++ bea.web/Controllers/API/AdShareAPIController.cs
@@ -0,0 +1,27 @@
+﻿using Facebook;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace Bea.Web.Controllers.API
+{
+    public class AdShareAPIController : ApiController
+    {
+
+        public HttpResponseMessage GetFacebookInfo()
+        {
+            HttpResponseMessage response;
+            var client = new FacebookClient();
+            dynamic me = client.Get("totten");
+            string firstName = me.first_name;
+            response = Request.CreateResponse(HttpStatusCode.OK, firstName);
+            return response;
+        }
+
+    }
+}
