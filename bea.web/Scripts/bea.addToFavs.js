04054058d91e520e78f1b633d5388c7c109f61f5
tekinico <tekinico@gmail.com>
2013-05-31 18:41:44 +1100

    modified add to favorites link to work. updated details view style

---
+++ bea.web/Scripts/bea.addToFavs.js
@@ -0,0 +1,14 @@
+﻿$(function () {
+    $('#bookmarkme').click(function () {
+        if (window.sidebar && window.sidebar.addPanel) { // Mozilla Firefox Bookmark
+            //window.sidebar.addPanel(document.title,window.location.href,''); -- do nothing
+        } else if(window.external && window.external.AddFavorite) { // IE Favorite
+            window.external.AddFavorite(location.href,document.title); 
+        } else if(window.opera && window.print) { // Opera Hotlist
+            this.title = document.title;
+            return true;
+        } else { // webkit - safari/chrome
+            alert('Votre naviguateur ne nous permet pas d\'ajouter cette page pour vous ! \n\rMais vous pouvez l\'ajouter vous même en utilisant le raccourci [' + (navigator.userAgent.toLowerCase().indexOf('mac') != -1 ? 'Command/Cmd' : 'CTRL') + ' + D] .');
+        }
+    });
+});
