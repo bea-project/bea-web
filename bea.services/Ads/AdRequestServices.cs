0bdb395c1acfdf6282322807256212771e9d62f5
tekinico <tekinico@gmail.com>
2013-06-01 11:31:41 +1100

    resolved bug on Request Ads email template

--- bea.services/Ads/AdRequestServices.cs
+++ bea.services/Ads/AdRequestServices.cs
@@ -15,6 +15,7 @@ namespace Bea.Services.Ads
         private readonly IAdRepository _adRepository;
         private readonly IEmailServices _emailServices;
         private readonly ITemplatingService _templatingServices;
+        private readonly IApplicationSettingsProvider _settingsProvider;
 
         public AdRequestServices(IAdRepository adRepository, IEmailServices emailServices, ITemplatingService templatingServices)
         {
@@ -28,21 +29,29 @@ namespace Bea.Services.Ads
             AdRequestModel result = new AdRequestModel();
             result.Email = model.Email;
             result.CanRequestAd = false;
+
             List<BaseAd> ads = _adRepository.GetAdsByEmail(model.Email).ToList();
             if (ads.Count == 0)
             {
                 result.InfoMessage = "Cette adresse email ne correspond aucune annonce.";
                 return result;
             }
+
             IDictionary<String, String> data = new Dictionary<String, String>();
             data.Add("name", ads[0].CreatedBy.Firstname);
             data.Add("adCount", ads.Count.ToString());
-            String subject = "BEA Vos Annonces";
+            data.Add("websiteAddress", _settingsProvider.WebsiteAddress);
+            data.Add("websiteName", _settingsProvider.WebsiteName);
+
             IDictionary<String, object[]> list = new Dictionary<String, object[]>();
             list.Add("ads", ads.ToArray());
+
+            String subject = "BEA Vos Annonces";
             String body = _templatingServices.GetTemplatedDocument("AdsRequestEmail.vm", data, list);
             _emailServices.SendEmail(subject, body, model.Email, "no-reply@bea.nc");
-            result.InfoMessage = "Un email vient de vous etre envoye avec la liste de vos annonces";
+            
+            result.InfoMessage = "Un email vient de vous etre envoyé avec la liste de vos annonces";
+            
             return result;
         }
     }
